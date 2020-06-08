d7a5e32bf1ae1b7c6bdbb40c0c28b542d04e93c8
tm-pham <thi.mui.pham@posteo.de>
2020-06-08 20:34:13 +0100

    Initial commit

---
+++ nosocomialtransmission_runfile.R
@@ -0,0 +1,30 @@
+# =============================================================================#
+# NOSOCOMIAL TRANSMISSION
+# Run file for STAN code
+# =============================================================================#
+library(rstan)
+setwd("/Users/tm-pham/PhD/covid-19/nosocomialtransmission/stochasticepidemicmodel/stan")
+sim_data <- readRDS("sim_data.RDS")
+
+# Data
+pDshape <- 5                        # Shape for gamma distribution for dispersion parameter for transmission process
+pDrate <- 0.5                       # Rate for gamma distribution for dispersion parameter for transmission process
+
+pDshape_obs <- 5                    # Shape for gamma distribution for dispersion parameter for observation process
+pDrate_obs <- 0.5                   # Rate for gamma distribution for dispersion parameter for observation process
+
+fshape <- 5                         # Shape for gamma distribution for transmission parameters
+frate <-  0.5                       # Rate for gamma distribution for transmission parameters
+
+sim_data <- append(sim_data, list(pDshape=pDshape, pDrate=pDrate, pDshape_obs=pDshape_obs, pDrate_obs=pDrate_obs, fshape=fshape, frate=frate)) 
+
+fit <- stan(
+  file = "nosocomialtransmission.stan",  # Input model version here 
+  data = sim_data,                  # named list of data defined in metareg_define_data.R
+  chains = 4,                            # number of Markov chains
+  warmup = 1000,                         # number of warmup iterations per chain
+  iter = 200000,                         # total number of iterations per chain
+  cores = 4,                             # number of cores (use one per chain)
+  refresh = 1000,                        # no of runs at which progress is shown
+  control = list(max_treedepth = 15, adapt_delta=0.99)
+)
