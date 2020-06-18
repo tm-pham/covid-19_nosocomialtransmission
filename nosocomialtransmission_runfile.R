09a677c297f95b27c1dab59b17782c0c51d23b86
tm-pham <thi.mui.pham@posteo.de>
2020-06-18 12:07:13 +0200

    Minor change

--- nosocomialtransmission_runfile.R
+++ nosocomialtransmission_runfile.R
@@ -27,35 +27,8 @@ sim_data <- append(sim_data, list(eps=eps,
                                   f_mu=f_mu, f_sigma=f_sigma))
 
 fit <- stan(
-  file = "nosocomialtransmissionv3.stan",  # Input model version here 
-  data = sim_data,                       # named list of data defined in metareg_define_data.R
-  chains = 1,                            # number of Markov chains
-  warmup = 100,                         # number of warmup iterations per chain
-  iter = 1000,                         # total number of iterations per chain
-  cores = 1,                             # number of cores (use one per chain)
-  refresh = 1,                        # no of runs at which progress is shown
-  control = list(max_treedepth = 15, adapt_delta=0.99)
-)
-
-init_fn <- function(){
-  return(list(f_pU_hcw=f_mu[1],
-              f_pU_p=f_mu[2],
-              f_p_hcw=f_mu[3],
-              f_p_p=f_mu[4],
-              f_hcw_hcw=f_mu[5],
-              f_hcw_p=f_mu[6],
-              I_hcwU=sim_data$I_hcwU0_array,
-              I_pU=sim_data$I_pU0_array,
-              new_symptomatic_hcw=sim_data$new_symptomatic_hcw0,
-              new_symptomatic_pat=sim_data$new_symptomatic_pat0,
-              discharge_dead_pat=sim_data$discharged_dead_pat0)
-  )
-}
-
-fit <- stan(
   file = "nosocomialtransmissionv3.stan",  # Input model version here
   data = sim_data,                       # named list of data defined in metareg_define_data.R
-  init = init_fn,
   chains = 1,                            # number of Markov chains
   warmup = 100,                         # number of warmup iterations per chain
   iter = 1000,                         # total number of iterations per chain
@@ -63,6 +36,34 @@ fit <- stan(
   refresh = 1,                        # no of runs at which progress is shown
   control = list(max_treedepth = 15, adapt_delta=0.99)
 )
+
+# fit <- stan(
+#   file = "nosocomialtransmissionv3.stan",  # Input model version here 
+#   data = sim_data,                       # named list of data defined in metareg_define_data.R
+#   chains = 1,                            # number of Markov chains
+#   warmup = 100,                         # number of warmup iterations per chain
+#   iter = 1000,                         # total number of iterations per chain
+#   cores = 1,                             # number of cores (use one per chain)
+#   refresh = 1,                        # no of runs at which progress is shown
+#   control = list(max_treedepth = 15, adapt_delta=0.99)
+# )
+# 
+# init_fn <- function(){
+#   return(list(f_pU_hcw=f_mu[1],
+#               f_pU_p=f_mu[2],
+#               f_p_hcw=f_mu[3],
+#               f_p_p=f_mu[4],
+#               f_hcw_hcw=f_mu[5],
+#               f_hcw_p=f_mu[6],
+#               I_hcwU=sim_data$I_hcwU0_array,
+#               I_pU=sim_data$I_pU0_array,
+#               new_symptomatic_hcw=sim_data$new_symptomatic_hcw0,
+#               new_symptomatic_pat=sim_data$new_symptomatic_pat0,
+#               discharge_dead_pat=sim_data$discharged_dead_pat0)
+#   )
+# }
+
+
 
 
 
